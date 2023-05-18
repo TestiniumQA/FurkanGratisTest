11b27ff46cca3990378bb200503599a3ad20fc4c
cengizhanonce <cengizhan.once@testinium.com>
2023-05-18 14:22:55 +0300

    second

--- src/test/java/com/testinium/HookImpl.java
+++ src/test/java/com/testinium/HookImpl.java
@@ -137,10 +137,11 @@ public class HookImpl {
                 capabilities.setCapability("usePrebuiltWDA",true);
                 capabilities.setCapability("useNewWDA", true);
                 capabilities.setCapability("autoAcceptAlerts",false);
-                capabilities.setCapability("appium:[skipDeviceInitialization]",true);
+                capabilities.setCapability("appium:[waitForIdleTimeout]",3);
+                capabilities.setCapability("appium:settings[animationCoolOffTimeout]",10);
+                capabilities.setCapability("appium:[waitForQuiescence]",false);
                 capabilities.setCapability("appium:[skipServerInstallation]",true);
-                capabilities.setCapability("appium:[ignoreUnimportantViews]",true);
-                capabilities.setCapability("appium:settings[waitForIdleTimeout]",10);
+                capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 3);
                 appiumDriver = new IOSDriver(new URL(hubURL), capabilities);
             }
         }
