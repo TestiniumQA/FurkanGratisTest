fa0d67ed350ca76c283aace9c51cf086bd436dc1
Burhan1925 <burhanaydiniu@gmail.com>
2023-06-20 08:56:40 +0300

    updating hook.imp

--- src/test/java/com/testinium/HookImpl.java
+++ src/test/java/com/testinium/HookImpl.java
@@ -134,7 +134,7 @@ public class HookImpl {
                 capabilities.setCapability("bundleId", "com.pharos.Gratis");
                 capabilities.setCapability("usePrebuiltWDA",true);
                 capabilities.setCapability("useNewWDA", true);
-                capabilities.setCapability("autoAcceptAlerts",true);
+                capabilities.setCapability("autoAcceptAlerts",false);
                 capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 5);
                 appiumDriver = new IOSDriver(new URL(hubURL), capabilities);
             }
