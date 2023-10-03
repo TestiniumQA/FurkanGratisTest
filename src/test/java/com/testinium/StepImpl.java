f259481ad9c3de6178c2c10706a919343b88b43d
mehmetuludag <mehmet.uludag@testinium.com>
2023-10-03 16:19:23 +0300

    wait update

--- src/test/java/com/testinium/StepImpl.java
+++ src/test/java/com/testinium/StepImpl.java
@@ -1406,7 +1406,7 @@ public class StepImpl extends HookImpl {
 
         if (mobileElement != null) {
 
-            doesElementExistByKey(key, 5);
+            doesElementExistByKey(key, 2);
             findElementByKey(key).click();
             logger.info(key + "elemente tıkladı");
 
@@ -1418,13 +1418,13 @@ public class StepImpl extends HookImpl {
         logger.info("element varsa verilen tıkla girdi");
         MobileElement mobileElement;
 
-        waitBySecond(2);
+        waitBySecond(1);
 
         mobileElement = findElementByKeyWithoutAssert(key);
 
         if (mobileElement != null) {
 
-            doesElementExistByKey(key, 5);
+            doesElementExistByKey(key, 2);
             findElementByKey(key).click();
             findElementByKey(key2).click();
             logger.info(key + "elemente tıkladı");
@@ -1446,7 +1446,7 @@ public class StepImpl extends HookImpl {
 
         if (mobileElement != null) {
 
-            doesElementExistByKey(key, 5);
+            doesElementExistByKey(key, 2);
             findElementByKey(key).click();
             logger.info(key + "elemente tıkladı");
 
