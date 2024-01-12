88d250c2d6fc7e0784f77bd0d2dff49fe04fc973
SAHABT <operasyon@testinium.com>
2024-01-12 13:36:04 +0000

    Update HookImpl.java

--- src/test/java/com/testinium/HookImpl.java
+++ src/test/java/com/testinium/HookImpl.java
@@ -110,7 +110,7 @@ public class HookImpl {
                         .setCapability(AndroidMobileCapabilityType.APP_ACTIVITY,
                                 "com.app.gratis.ui.splash.SplashActivity");
                 capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"uiautomator2");
-                capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
+                capabilities.setCapability(MobileCapabilityType.NO_RESET, false);
                 capabilities.setCapability(MobileCapabilityType.FULL_RESET, false);
                 capabilities.setCapability(AndroidMobileCapabilityType.AUTO_GRANT_PERMISSIONS, true);
                 capabilities.setCapability("unicodeKeyboard", true);
