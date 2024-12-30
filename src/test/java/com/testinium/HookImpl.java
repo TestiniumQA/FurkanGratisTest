bea724296c899c3108a659886f1b669b16c13d8a
Furkan Kartal <Furkan.kartal@testinium.com>
2024-12-30 12:58:02 +0300

    denme

--- src/test/java/com/testinium/HookImpl.java
+++ src/test/java/com/testinium/HookImpl.java
@@ -92,7 +92,7 @@ public class HookImpl {
 
             }
         } else {
-            String hubURL = "https://hubclouddev.testinium.com/wd/hub";
+            String hubURL = "http://hub-devcluster.testinium.io:4444/wd/hub";
             DesiredCapabilities capabilities = new DesiredCapabilities();
 
             System.out.println("key:" + System.getenv("key"));
